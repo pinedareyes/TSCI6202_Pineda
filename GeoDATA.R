8e5a3a2d6ac42c8007426bc4f26a553d5a3350e2
AnaPineda <apineda@uthsc.edu>
2026-04-23 13:35:43 -0500

    04/16

--- GeoDATA.R
+++ GeoDATA.R
@@ -30,3 +30,22 @@ ggplot(data = FQHC_C2) +
   geom_sf(data = FQHC_SF, color="pink", size=1.5, alpha=.3)+
   scale_fill_viridis_c()
 
+#Creating Synthetic Data of Street Addresses
+street_address<-data.frame(full_address=c("7703 Floyd Curl Dr, San Antonio, TX 78258","1 Haven for Hope Way, San Antonio, TX 78207", "300 Alamo Plaza, San Antonio, TX 78205", "1402 Broadway St, Galveston, TX 77550", "411 Elm St, Dallas, TX 75202", "604 Brazos St, Austin, TX 78701", "2515 W 5th St, Irving, TX 75060", "100 Lady Bird Lane, Johnson City, TX 78636", "1412 W Ohio Ave, Midland, TX 79701", "1217 W Sam Rayburn Dr, Bonham, TX 75418", "3700 Hogge Dr, Parker, TX 75002", "2101 Ross Ave, Dallas, TX 75201", "2414 Rosedale St, Houston, TX 77004"))
+#Obtain cordinantes for the addressses
+geocoded_street_address<-geocode(street_address, address=full_address, method="osm")
+#Converting coordinates to shape file
+shape_file_street_address<-st_as_sf(geocoded_street_address,coords=c("long","lat"),crs=4326)
+
+ggplot(data=FQHC_C2)+
+  geom_sf(fill="purple")+
+  geom_sf(data=FQHC_SF, color="pink", size=1.5, alpha=0.3)+
+  geom_sf(data=shape_file_street_address, color="blue")
+
+geom_sf(Data=shapefile_addresses, color='green')
+#Interactive Mapping
+tmap_mode("view")
+tm_shape(FQHC_C2)+
+  tm_polygons(fill="clinic_count_raw")+
+  tm_shape(shape_file_street_address)+
+  tm_dots(fill="darkgreen", fill_alpha=0.5, size=0.2)
